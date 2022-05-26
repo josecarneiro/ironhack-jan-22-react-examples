8a693300836aa0f0c520a486f12a424e40bc59f4
José Carneiro <contact@josecarnei.ro>
2022-05-26 18:30:58 +0100

    Add iteration

--- react-conditionals-iteration/src/App.jsx
+++ react-conditionals-iteration/src/App.jsx
@@ -10,9 +10,33 @@ console.log((value && 'Its true') || 'Its false');
 
 function App() {
   const [displayMessage, setDisplayMessage] = useState(false);
+  const [students, setStudents] = useState([
+    { id: '1', name: 'Sofiane', location: 'Morocco' },
+    { id: '2', name: 'Johanna', location: 'Berlin' },
+    { id: '3', name: 'Andrés', location: 'Southampton' }
+  ]);
 
   const handleToggleMessageClick = () => {
     setDisplayMessage(!displayMessage);
+  };
+
+  const handleStudentRemoval = (id) => {
+    /* We should never directly mutate state */
+    /* We should not call .push(), .pop(), .splice(), .shift(), .unshift() */
+    /* We can call .filter(), .map(), .reduce(), .slice(), etc */
+    /* https://doesitmutate.xyz */
+    const remainingStudents = students.filter((student) => {
+      return student.id !== id;
+    });
+    setStudents(remainingStudents);
+    /*
+    To add to an array stored in component state
+    const updatedStudents = [
+      ...students,
+      { id: '4', name: 'Ezgi', location: 'Berlin' }
+    ];
+    setStudents(updatedStudents);
+    */
   };
 
   return (
@@ -21,6 +45,18 @@ function App() {
         {displayMessage ? 'Hide' : 'Show'} Message
       </button>
       {displayMessage && <h1>This is a secret message</h1>}
+      <ul>
+        {students.map((student) => {
+          return (
+            <li key={student.id}>
+              {student.name} is based in {student.location}{' '}
+              <button onClick={() => handleStudentRemoval(student.id)}>
+                ✅
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
